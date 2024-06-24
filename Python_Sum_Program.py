405df061881c2e18af9a99ee8059020d558c03e8
monayemchy <monayemchy@gmail.com>
2024-06-25 00:57:12 +0600

    Class1_Program

---
+++ Python_Sum_Program.py
@@ -0,0 +1,9 @@
+#Variable declaration
+firstNum = int(input("Enter the first number: "))
+secondNum = int(input("Enter the second number: "))
+
+#Sum calculation
+sum = firstNum+secondNum
+
+#Show output
+print("Result of sum is: ",sum)
